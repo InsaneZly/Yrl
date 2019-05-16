b7b685851f06645833b05ff41590a18072ef3b9d
yaowan4tong <451930441@qq.com>
2019-05-16 09:40:54 +0800

    a

---
+++ cl201831980103/Controllers/MyController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace cl201831980103.Controllers
+{
+    public class MyController : Controller
+    {
+        // GET: My
+        public ActionResult Index()
+        {
+            ViewBag.aaa = "这是我自定义控制器My中的Index动作的数据";
+            return View();
+        }
+    }
+}
---
+++ cl201831980103/Models/MyDb.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace cl201831980103.Models
+{
+    public class MyDb : DbContext
+    {
+        public MyDb() : base("name=DefaultConnection")
+        {
+
+        }
+        public DbSet<cl201831980103.Models.student> student { get; set; }
+       
+
+
+    }
+}
